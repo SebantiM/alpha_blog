df175a841302c10bda3cf62e32f7757a93be02a1
SebantiM <sebanti_mofokeng@yahoo.com>
2018-11-26 20:59:25 +0200

    extract redundency and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,18 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
 
-  def create
+  def edit
+  end
 
+  def create
     @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was successfully created"
@@ -14,11 +22,28 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+      if @article.update(article_params)
+      flash[:notice] = "Article was sucessfully updated"
+      redirect_to  article_path(@article)
+    else
+      render "edit"
+    end
+  end
+
 def show
-  @article = Article.find(params[:id])
 end
 
-  private
+def destroy
+    @article.destroy
+  flash[:notice] = "Article was sucessfully deleted"
+  redirect_to articles_path
+end
+
+private
+  def set_article
+      @article = Article.find(params[:id])
+  end
   def article_params
     params.require(:article).permit(:title, :description)
   end
